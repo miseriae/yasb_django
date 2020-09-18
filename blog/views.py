ed682d8f73d83fbc85c06e519b9fb21135e2ec82
miseriae <zuyev.frm@gmail.com>
2020-09-19 00:24:28 +0300

    Add basic blog functionality

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,36 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 from .models import Post
+from .forms import PostForm, PostUpdateForm
 
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-created_date']
 
 
 class ArticleDetailView(DetailView):
     model = Post
-    template_name = 'article_detail.html'
+    template_name = 'article-detail.html'
 
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add-post.html'
-    fields = '__all__'
+
+
+class UpdatePostView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostUpdateForm 
+    template_name = 'update-post.html'
+
+
+class DeletePostView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'delete-post.html'
+    success_url = reverse_lazy('home')
