3ef5d85121ffa020cd06a064925f4ad804c268be
miseriae <zuyev.frm@gmail.com>
2020-09-21 21:00:13 +0300

    Create user app, registration/login functions

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import UserRegisterView
+from django.contrib.auth import views as auth_views
+
+from .views import UserRegisterView, UserUpdateView, ChangePasswordView
 
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('edit-profile/', UserUpdateView.as_view(), name='edit-profile'),
+    # path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html'))
+    path('password/', ChangePasswordView.as_view(template_name='registration/change-password.html'))
 ]
